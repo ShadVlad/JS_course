8115426298b006e1057eb9ca94b794f67ca3d6f9
Vladimir <wowchik.shadrin@yandex.ru>
2021-04-02 17:50:57 +0300

    homework lesson 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,19 @@
-let money;
-let incom;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 50000;
+let incom = "Freelance";
+let addExpenses = "Интернет, такси, коммуналка";
+let deposit = true;
+let mission = 1000000;
+let period = 6;
 
-alert("Hello, teacher!");
-console.log("Это сообщение в консоли....");
+console.log("Переменная 'money' имеет тип '", typeof money, "'");
+console.log("Переменная 'incom' имеет тип '", typeof incom, "'");
+console.log("Переменная 'deposit' имеет тип '", typeof deposit, "'");
+
+console.log("Длина строки '" + addExpenses + "' = " + addExpenses.length);
+
+console.log("Период равен " + period + " месяцев ");
+console.log("Цель заработать " + mission + " рублей");
+
+console.log(addExpenses.toLowerCase().split(","));
+let budgetDay = money / 30;
+console.log("Доход за день " + budgetDay.toFixed(2) + " рублей");
