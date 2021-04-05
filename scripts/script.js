5c7f58ec1b75f65d9f564e856d1b72a76f5d627c
Vladimir <wowchik.shadrin@yandex.ru>
2021-04-05 17:06:44 +0300

    homework lesson 2

--- scripts/script.js
+++ scripts/script.js
@@ -18,40 +18,8 @@ console.log("Цель заработать " + mission + " рублей");
 
 console.log(addExpenses.toLowerCase().split(","));
 
+let budgetDay = money / 30;
+console.log("Доход за день " + budgetDay.toFixed(2) + " рублей");
+
 money = prompt("Ваш месячный доход?");
-//console.log("~ money", money);
-
-addExpenses = prompt(
-  "Перечислите возможные расходы за рассчитываемый период через запятую"
-);
-//console.log("~ addExpenses", addExpenses);
-
-deposit = confirm("Есть ли у вас депозит в банке?");
-//console.log("~ deposit", deposit);
-
-let expenses1 = prompt("Введите обязательную статью расходов?");
-let amount1 = prompt("Во сколько это обойдется?");
-let expenses2 = prompt("Введите обязательную статью расходов?");
-let amount2 = prompt("Во сколько это обойдется?");
-
-let budgetMonth = money - amount1 - amount2;
-console.log("~ Бюджет на месяц =", budgetMonth);
-
-let missionLong = mission / budgetMonth;
-console.log(
-  "~ Вы достигните цели через " + Math.ceil(missionLong) + " месяцев"
-);
-
-let budgetDay = budgetMonth / 30;
-//console.log("Доход за день " + budgetDay.toFixed(2) + " рублей");
-console.log("Доход за день " + Math.floor(budgetDay) + " рублей");
-
-if (budgetDay > 1200) {
-  alert("У вас высокий уровень дохода");
-} else if (budgetDay > 600) {
-  alert("У вас средний уровень дохода");
-} else if (budgetDay >= 0) {
-  alert("К сожалению у вас уровень дохода ниже среднего");
-} else {
-  alert("Что то пошло не так");
-}
+console.log("~ money", money);
